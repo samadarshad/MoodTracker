be866ac50bdc25062301fc096460c17cf71137a7
Samad Arshad <samad.arshad@ev.energy>
2022-12-26 10:58:31 +0000

    Context

---
+++ src/App.provider.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { createContext } from 'react';
+import { MoodOptionWithTimestamp, MoodOptionType } from './types';
+
+type AppContextType = {
+  moodList: MoodOptionWithTimestamp[];
+  handleSelectMood: (mood: MoodOptionType) => void;
+};
+const AppContext = createContext<AppContextType>({
+  moodList: [],
+  handleSelectMood: () => {},
+});
+
+export const AppProvider: React.FC = ({ children }) => {
+  const [moodList, setMoodList] = React.useState<MoodOptionWithTimestamp[]>([]);
+
+  const handleSelectMood = React.useCallback((selectedMood: MoodOptionType) => {
+    setMoodList(current => [
+      ...current,
+      { mood: selectedMood, timestamp: Date.now() },
+    ]);
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ moodList, handleSelectMood }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => React.useContext(AppContext);
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { MoodPicker } from '../components/MoodPicker';
-import { MoodOptionType, MoodOptionWithTimestamp } from '../types';
-import { format } from 'date-fns';
-import { MoodItemRow } from '../components/MoodItemRow';
+import { useAppContext } from '../App.provider';
 
 export const Home: React.FC = () => {
-  const [moodList, setMoodList] = React.useState<MoodOptionWithTimestamp[]>([]);
-
-  const handleSelectMood = React.useCallback((selectedMood: MoodOptionType) => {
-    setMoodList(current => [
-      ...current,
-      { mood: selectedMood, timestamp: Date.now() },
-    ]);
-  }, []);
+  const appContext = useAppContext();
 
   return (
     <View style={styles.container}>
-      <MoodPicker handleSelectMood={handleSelectMood} />
-      {moodList.map(item => (
-        <MoodItemRow item={item} key={item.timestamp} />
-      ))}
+      <MoodPicker handleSelectMood={appContext.handleSelectMood} />
     </View>
   );
 };
