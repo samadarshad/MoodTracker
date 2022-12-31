25d74b161d6da35e2703c748202f5eac228a965d
Samad Arshad <samad.arshad@ev.energy>
2022-12-31 12:26:00 +0000

    Analytics

--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { useAppContext } from '../App.provider';
+import { VictoryPie } from 'victory-native';
+import { groupBy } from 'lodash';
+import { theme } from '../theme';
 
 export const Analytics: React.FC = () => {
+  const appContext = useAppContext();
+  const data = Object.entries(groupBy(appContext.moodList, 'mood.emoji')).map(
+    ([key, value]) => ({
+      x: key,
+      y: value.length,
+    }),
+  );
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <VictoryPie
+        labelRadius={80}
+        radius={150}
+        innerRadius={50}
+        colorScale={[
+          theme.colorPurple,
+          theme.colorLavender,
+          theme.colorBlue,
+          theme.colorGrey,
+          theme.colorWhite,
+        ]}
+        style={{ labels: { fontSize: 30 } }}
+        data={data}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
